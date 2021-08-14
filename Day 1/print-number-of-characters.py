1bf450a28309d88a226b103cd4fdbfdaf14244dd
João Oliveira <38820667+swokyisalreadytaken@users.noreply.github.com>
2021-08-14 04:16:00 +0100

    Add files via upload

---
+++ Day 1/print-number-of-characters.py
@@ -0,0 +1,16 @@
+# Ask for the user name and assigns to name
+name = input("What's your name? ")
+
+# Counts the number of characters of the name
+print(len(name))
+
+# Other option, same result
+print(len(input("What's your name? ")))
+
+
+
+
+
+
+
+
