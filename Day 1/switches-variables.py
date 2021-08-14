30bdb8aaf796d70b74271803c2bdd3adfabeeb63
João Oliveira <38820667+jotavare@users.noreply.github.com>
2021-08-14 23:05:33 +0100

    feat: add day 1-2 exercises (band name generator, BMI calculator, and more)

---
+++ Day 1/switches-variables.py
@@ -0,0 +1,15 @@
+# can't change the code bellow
+a = input("a: ")
+b = input("b: ")
+
+# assign new variables and change the order
+first = a
+second = b
+b = first
+a = second
+
+# print teh new variables
+print("a: " + a)
+print("b: " + b)
+
+
