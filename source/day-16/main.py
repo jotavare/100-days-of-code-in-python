6f62790ba142989803890bd04976aab0afd3330f
jotavare <jpoliveira.mm@gmail.com>
2024-05-29 13:08:31 +0100

    added day 16

---
+++ source/day-16/main.py
@@ -0,0 +1,16 @@
+# import another_module
+# print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+import prettytable
+
